f1347cc1d8add3f637cbf2f08461fceeb1ca36ad
Youngbin <ybkim512@siwonschool.com>
2024-03-14 17:09:41 +0900

    initial

--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "MSCognitiveServiceSpeechSPM",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MSCognitiveServiceSpeechSPM",
-            targets: ["MSCognitiveServiceSpeechSPM"]),
+            targets: ["MSCognitiveServiceSpeechSPM", "MicrosoftCognitiveServicesSpeech"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -19,5 +20,8 @@ let package = Package(
         .testTarget(
             name: "MSCognitiveServiceSpeechSPMTests",
             dependencies: ["MSCognitiveServiceSpeechSPM"]),
+        .binaryTarget(
+            name: "MicrosoftCognitiveServicesSpeech",
+            path: "Frameworks/MicrosoftCognitiveServicesSpeech.xcframework")
     ]
 )
